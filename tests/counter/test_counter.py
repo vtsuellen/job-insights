52092a1984ee7a9f78df81b0df93bc27ff95a4dd
vtsuellen <vtsuellen@hotmail.com>
2024-01-23 16:03:31 -0300

    test: Count Ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+
+PATH = "data/jobs.csv"
 
 
 def test_counter():
-    raise
+    assert count_ocurrences(PATH, "Python") == 1639
+    assert count_ocurrences(PATH, "Javascript") == 122
