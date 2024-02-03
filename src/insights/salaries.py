372d1aa522d59d62f11ed44d9848cbcf6be8fe38
vtsuellen <vtsuellen@hotmail.com>
2024-02-02 22:07:16 -0300

    feat: matches salary range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -8,22 +8,35 @@ class ProcessSalaries(ProcessJobs):
 
     def get_max_salary(self) -> int:
         salaries_list = list(
-            int(job['max_salary'])
+            int(job["max_salary"])
             for job in self.jobs_list
-            if job['max_salary'].isdigit()
+            if job["max_salary"].isdigit()
         )
         return max(salaries_list)
 
     def get_min_salary(self) -> int:
         salaries_list = list(
-            int(job['min_salary'])
+            int(job["min_salary"])
             for job in self.jobs_list
-            if job['min_salary'].isdigit()
+            if job["min_salary"].isdigit()
         )
         return min(salaries_list)
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
-        pass
+        try:
+            min_salary = int(job["min_salary"])
+            max_salary = int(job["max_salary"])
+            salary = int(salary)
+            if min_salary > max_salary:
+                raise ValueError(
+                    "Salario minimo deve ser menor que o salario maximo"
+                )
+            if min_salary <= salary <= max_salary:
+                return True
+
+            return False
+        except (TypeError, KeyError):
+            raise ValueError()
 
     def filter_by_salary_range(
         self, jobs: List[dict], salary: Union[str, int]
